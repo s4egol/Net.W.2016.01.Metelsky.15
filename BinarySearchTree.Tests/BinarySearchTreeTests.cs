1c485690be672b5006d236f5f8be50db2a68109a
Vanya <vanya.metelsky@gmail.com>
2016-11-25 02:42:48 +0300

    BinarySearchTree done

---
+++ BinarySearchTree.Tests/BinarySearchTreeTests.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Task2;
+using Task1;
+
+
+namespace BinarySearchTree.Tests
+{
+    [TestFixture]
+    public class BinarySearchTreeTests
+    {
+        [Test]
+        public void Contains_returnedValues()
+        {
+            BinarySearchTree<int> tree = new BinarySearchTree<int>();
+
+            for (int i = 0; i < 5; i++)
+            {
+                tree.Add(i);
+            }
+
+            Assert.AreEqual(tree.Contains(4), true);
+        }
+
+        [Test]
+        public void Preorder_returnedValues()
+        {
+            int[] input = new[] {19, 33, 31, 35, 13, 17, 18, 15, 3};
+            int[] output = new[] {19, 13, 3, 17, 15, 18, 33, 31, 35};
+
+            BinarySearchTree<int> tree = new BinarySearchTree<int>(input);
+
+            int index = 0;
+            foreach (var value in tree.Preorder())
+            {
+                Assert.AreEqual(output[index], value);
+                index++;
+            }
+        }
+
+        [Test]
+        public void Inorder_returnedValues()
+        {
+            int[] input = new[] { 19, 33, 31, 35, 13, 17, 18, 15, 3 };
+            int[] output = new[] { 3, 13, 15, 17, 18, 19, 31, 33, 35 };
+
+            BinarySearchTree<int> tree = new BinarySearchTree<int>(input);
+
+            int index = 0;
+            foreach (var value in tree.Inorder())
+            {
+                Assert.AreEqual(output[index], value);
+                index++;
+            }
+        }
+
+        [Test]
+        public void Postorder_returnedValues()
+        {
+            int[] input = new[] { 19, 33, 31, 35, 13, 17, 18, 15, 3 };
+            int[] output = new[] { 3, 15, 18, 17, 13, 31, 35, 33, 19 };
+
+            BinarySearchTree<int> tree = new BinarySearchTree<int>(input);
+
+            int index = 0;
+            foreach (var value in tree.Postorder())
+            {
+                Assert.AreEqual(output[index], value);
+                index++;
+            }
+        }
+
+        public class ComparatorInt : Comparer<int>
+        {
+            public override int Compare(int x, int y)
+            {
+                return x.CompareTo(y);
+            }
+        }
+
+        [Test]
+        public void ComparerInt_returnedValues()
+        {
+            BinarySearchTree<int> tree = new BinarySearchTree<int>(new ComparatorInt());
+            List<int> list = new List<int>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                tree.Add(i);
+                list.Add(i);
+            }
+
+            list.Sort(new ComparatorInt());
+            CollectionAssert.AreEqual(list, tree.Inorder());
+        }
+
+        public class ComparatorString : Comparer<string>
+        {
+            public override int Compare(string x, string y) => String.Compare(x, y, StringComparison.Ordinal);
+        }
+
+        [Test]
+        public void ComparerString_returnedValues()
+        {
+            BinarySearchTree<string> tree = new BinarySearchTree<string>(new ComparatorString());
+            List<string> list = new List<string>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                tree.Add(i.ToString());
+                list.Add(i.ToString());
+            }
+
+            list.Sort(new ComparatorString());
+            CollectionAssert.AreEqual(list, tree.Inorder());
+        }
+
+        public struct Point
+        {
+            public int x, y;
+
+            public Point(int x, int y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+        }
+        public class ComparatorPoint : Comparer<Point>
+        {
+            public override int Compare(Point x, Point y)
+            {
+                return (x.x + 1).CompareTo(y.x + 1);
+            }
+        }
+
+        [Test]
+        public void ComparerPoint_returnedValues()
+        {
+            BinarySearchTree<Point> tree = new BinarySearchTree<Point>(new ComparatorPoint());
+            List<Point> list = new List<Point>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                tree.Add(new Point(i, i));
+                list.Add(new Point(i, i));
+            }
+
+            list.Sort(new ComparatorPoint());
+            CollectionAssert.AreEqual(list, tree.Inorder());
+        }
+
+        public class ComparatorBook : Comparer<Book>
+        {
+            public override int Compare(Book x, Book y)
+            {
+                return x.CompareTo(y);
+            }
+        }
+
+        [Test]
+        public void ComparerBook_returnedValues()
+        {
+            BinarySearchTree<Book> tree = new BinarySearchTree<Book>(new ComparatorBook());
+            List<Book> list = new List<Book>();
+
+            for (int i = 1; i < 10; i++)
+            {
+                tree.Add(new Book("title", i, 2000, "Second name"));
+                list.Add(new Book("title", i, 2000, "Second name"));
+            }
+
+            list.Sort(new ComparatorBook());
+            CollectionAssert.AreEqual(list, tree.Inorder());
+        }
+    }
+}
---
+++ Task2/BinarySearchTree.cs
@@ -0,0 +1,330 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    public class BinarySearchTree<T> : ICollection<T>
+    {
+        /// <summary>
+        /// Node of tree
+        /// </summary>
+        /// <typeparam name="TData">Type of data</typeparam>
+        private class Node<TData>
+        {
+            public TData Data { get; set; }
+            public Node<TData> Left { get; set; }
+            public Node<TData> Right { get; set; }
+
+            public Node(TData data)
+            {
+                Data = data;
+            } 
+        }
+
+        private Node<T> root;
+        private IComparer<T> comparer;
+        public int Count { get; private set; }
+        public bool IsReadOnly {
+            get { return false; }
+        }
+
+        public void Clear()
+        {
+            root = null;
+            Count = 0;
+        }
+
+        public bool Contains(T item)
+        {
+            var current = root;
+
+            while (current != null)
+            {
+                var result = comparer.Compare(item, current.Data);
+
+                if (result == 0)
+                    return true;
+                if (result < 0)
+                    current = current.Left;
+                else 
+                    current = current.Right;
+            }
+            return false;
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            if (ReferenceEquals(array, null))
+                throw new ArgumentException();
+
+            foreach (var value in this)
+                array[arrayIndex++] = value;
+        }
+
+        public BinarySearchTree(IComparer<T> comparer)
+        {
+            if (ReferenceEquals(comparer, null))
+                comparer = Comparer<T>.Default;
+            else
+                this.comparer = comparer;
+        }
+
+        public BinarySearchTree() : this(Comparer<T>.Default)
+        {
+            
+        }
+
+        public BinarySearchTree(IEnumerable<T> collection) : this(collection, Comparer<T>.Default)
+        {
+
+        }
+
+        public BinarySearchTree(IEnumerable<T> collection, IComparer<T> comparer) : this(comparer)
+        {
+            if (ReferenceEquals(collection, null))
+                throw new ArgumentNullException();
+
+            AddRange(collection);
+        }
+
+        public void AddRange(IEnumerable<T> collection)
+        {
+            if (ReferenceEquals(collection, null))
+                throw new ArgumentNullException();
+
+            foreach (var value in collection)
+                Add(value);
+        }
+
+        /// <summary>
+        /// Adding a new element into the tree
+        /// </summary>
+        /// <param name="value">New element</param>
+        public void Add(T value)
+        {
+            if (root == null)
+                root = new Node<T>(value);
+            else
+            {
+                AddElementRecursion(root, value);
+                Count++;
+            }
+        }
+
+        /// <summary>
+        /// Straight tree traversal
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<T> Preorder()
+        {
+            if (root == null)
+                yield break;
+
+            var stack = new Stack<Node<T>>();
+            stack.Push(root);
+            while (stack.Count != 0)
+            {
+                Node<T> node = stack.Pop();
+                yield return node.Data;
+
+                if (node.Right != null)
+                    stack.Push(node.Right);
+                if (node.Left != null)
+                    stack.Push(node.Left);
+            }
+        }
+
+        /// <summary>
+        /// Reverse traversal of the tree
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<T> Postorder()
+        {
+            if (root == null)
+                yield break;
+
+            var stack = new Stack<Node<T>>();
+            Node<T> node = root;
+
+            while (stack.Count > 0 || node != null)
+            {
+                if (node == null)
+                {
+                    node = stack.Pop();
+                    if (stack.Count > 0 && node.Right == stack.Peek())
+                    {
+                        stack.Pop();
+                        stack.Push(node);
+                        node = node.Right;
+                    }
+                    else
+                    {
+                        yield return node.Data;
+                        node = null;
+                    }
+                }
+                else
+                {
+                    if (node.Right != null)
+                        stack.Push(node.Right);
+                    stack.Push(node);
+                    node = node.Left;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Symmetrical tree traversal
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<T> Inorder()
+        {
+            if (root == null)
+                yield break;
+
+            var stack = new Stack<Node<T>>();
+            Node<T> node = root;
+
+            while (stack.Count > 0 || node != null)
+            {
+                if (node == null)
+                {
+                    node = stack.Pop();
+                    yield return node.Data;
+
+                    if (node.Right != null)
+                        node = stack.Pop();
+                    else
+                        node = null;
+                }
+                else
+                {
+                    if (node.Right != null)
+                        stack.Push(node.Right);
+                    stack.Push(node);
+                    node = node.Left;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Remove from the wood element
+        /// </summary>
+        /// <param name="value">Deleted item</param>
+        /// <returns>Removed or not</returns>
+        public bool Remove(T value)
+        {
+            if (root == null)
+                return false;
+
+            Node<T> current = root, parent = null;
+            int result;
+            do
+            {
+                result = comparer.Compare(value, current.Data);
+
+                if (result < 0)
+                {
+                    parent = current;
+                    current = current.Left;
+                }
+                if (result > 0)
+                {
+                    parent = current;
+                    current = current.Right;
+                }
+                if (current == null)
+                    return false;
+            } while (result != 0);
+
+            if (current.Right == null)
+            {
+                if (current == root)
+                    root = current.Left;
+                else
+                {
+                    result = comparer.Compare(current.Data, parent.Data);
+
+                    if (result < 0)
+                        parent.Left = current.Left;
+                    else
+                        parent.Right = current.Left;
+                }
+            }
+            else if (current.Right.Left == null)
+            {
+                current.Right.Left = current.Left;
+
+                if (current == root)
+                    root = current.Right;
+                else
+                {
+                    result = comparer.Compare(current.Data, parent.Data);
+                    if (result < 0)
+                        parent.Left = current.Right;
+                    else
+                        parent.Right = current.Right;
+                }
+            }
+            else
+            {
+                Node<T> min = current.Right.Left, prev = current.Right;
+
+                while (min.Left != null)
+                {
+                    prev = min;
+                    min = min.Left;
+                }
+                prev.Left = min.Right;
+                min.Left = current.Left;
+                min.Right = current.Right;
+
+                if (current == root)
+                    root = min;
+                else
+                {
+                    result = comparer.Compare(current.Data, parent.Data);
+
+                    if (result < 0)
+                        parent.Left = min;
+                    else
+                        parent.Right = min;
+                }
+            }
+            --Count;
+            return true;
+        }
+
+        private void AddElementRecursion(Node<T> currentNode, T newElement)
+        {
+            if (comparer.Compare(currentNode.Data, newElement) < 0)
+            {
+                if (ReferenceEquals(currentNode.Right, null))
+                    currentNode.Right = new Node<T>(newElement);
+                else
+                    AddElementRecursion(currentNode.Right, newElement);
+            }
+            else
+            {
+                if (ReferenceEquals(currentNode.Left, null))
+                    currentNode.Left = new Node<T>(newElement);
+                else
+                    AddElementRecursion(currentNode.Left, newElement);
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return Inorder().GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
